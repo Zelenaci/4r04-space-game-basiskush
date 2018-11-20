fbece1b49312e514689010c11f71e317268b4ab2
basiskush <31956786+basiskush@users.noreply.github.com>
2018-11-20 11:32:47 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python3
-# Soubor:  kameny.py
-# Datum:   06.11.2018 10:01
-# Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
-# Licence: GNU/GPL
-############################################################################
 import pyglet
 import random
 from math import sin, cos, radians, pi
+from pyglet.window.key import DOWN, UP, LEFT, RIGHT
 
 window = pyglet.window.Window(1000, 800)
 batch = pyglet.graphics.Batch()   # pro optimalizované vyreslování objektů
 
 
-class SpaceObject(object):
+class Stone(object):
 
-    def __init__(self, img_file,
-                 x=None, y=None,
-                 direction=None,
-                 speed=None, rspeed=None):
-
+    def __init__(self,x=None, y=None,direction=None,speed=None, rspeed=None):
         # nečtu obrázek
-        self.image = pyglet.image.load(img_file)
+        num = random.choice(range(0, 20))
+        self.image = pyglet.image.load('meteors/{}.png'.format(num))
+        
         # střed otáčení dám na střed obrázku
         self.image.anchor_x = self.image.width // 2
         self.image.anchor_y = self.image.height // 2
@@ -28,27 +21,17 @@ class SpaceObject(object):
         self.sprite = pyglet.sprite.Sprite(self.image, batch=batch)
 
         # pokud není atribut zadán vytvořím ho náhodně
-        self._x = x if x is not None else random.randint(0, window.width)
-        self._y = y if y is not None else random.randint(0, window.height)
+        self.x = x if x is not None else random.randint(0, window.width)
+        self.y = y if y is not None else random.randint(0, window.height)
         # musím správně nastavit polohu sprite
-        self.x = self._x
-        self.y = self._y
+        self.sprite.x = self.x
+        self.sprite.y = self.y
 
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self, new):
-        self._x = self.sprite.x = new
-
-    @property
-    def y(self):
-        return self._y
-
-    @y.setter
-    def y(self, new):
-        self._y = self.sprite.y = new
+        self.direction = direction if direction is not None else random.randint(0, 359)
+        # rychlost pohybu
+        self.speed = speed if speed is not None else random.randint(30, 180)
+        # rychlost otáčení
+        self.rspeed = rspeed if rspeed is not None else random.randint(-2, 5)
 
     def tick(self, dt):
         self.bounce()
@@ -58,10 +41,107 @@ class SpaceObject(object):
         self.sprite.x = self.x
         self.y += dt * self.speed * sin(pi / 2 - radians(self.direction))
         self.sprite.y = self.y
-        self.sprite.rotation += 0.01 * self.rspeed
+        self.sprite.rotation +=self.rspeed
+
+    def bounce(self):
+
+        # vzdálenost okraje a středu
+        rozmer = min(self.image.width, self.image.height)/2
+
+        if self.x + rozmer >= window.width:
+            self.direction = random.randint(190, 350)
+            return
+        if self.x - rozmer <= 0:
+            self.direction = random.randint(10, 170)
+            return
+        if self.y + rozmer >= window.height:
+            self.direction = random.randint(100, 260)
+            return
+        if self.y - rozmer <= 0:
+            self.direction = random.randint(-80, 80)
+            return
 
 
-a = SpaceObject('SpaceShooterRedux/PNG/playerShip1_red.png')
+
+class Lodka(object):
+    
+    def __init__(self):
+        #vybereme obrázku lodě
+        self.obrazek = pyglet.image.load("playerShip1_green.png")
+        
+        #střed otáčení toho obrázku umístíme do prostřed
+        self.obrazek.anchor_x = self.obrazek.width // 2
+        self.obrazek.anchor_y = self.obrazek.height // 2
+        
+        #z obrázku uděláme sprite
+        self.sprite =pyglet.sprite.Sprite(self.obrazek, batch=batch)
+        
+        self.sprite.rotation=60
+        self.speed= 200
+        self.x=500
+        self.y=400
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+    def tiktak(self,t):
+        global klavesy
+        self.okraj()
+        #ovládání lodičky
+        for data in klavesy:
+            if data == LEFT:
+                self.sprite.rotation -= 5
+            if data == RIGHT:
+                self.sprite.rotation += 5
+            if data == UP:
+                self.x = self.sprite.x + self.speed*t*sin(pi*self.sprite.rotation/180)
+                self.sprite.x=self.x
+                self.y = self.sprite.y + self.speed*t*cos(pi*self.sprite.rotation/180)
+                self.sprite.y=self.y
+            if data == DOWN:
+                self.x = self.sprite.x + self.speed*t*-sin(pi*self.sprite.rotation/180)
+                self.sprite.x = self.x
+                self.y = self.sprite.y + self.speed*t*-cos(pi*self.sprite.rotation/180)
+                self.sprite.y = self.y
+    
+    def okraj(self):
+        # vzdálenost okraje a středu
+        rozmer = min(self.obrazek.width, self.obrazek.height)/2
+        if self.x + rozmer >= window.width:
+            self.speed=0
+        if self.x - rozmer <= 0:
+            self.speed=0
+        if self.y + rozmer >= window.height:
+            self.speed=0
+        if self.y - rozmer <= 0:
+            self.speed=0
+
+            
+                                                                 
+     
+        
+        
+
+klavesy=set()        
+for o in range(1):
+    lod=Lodka()
+    pyglet.clock.schedule_interval(lod.tiktak, 1/ 30 )
+
+stones = []
+for i in range(5):
+    stone = Stone()
+    pyglet.clock.schedule_interval(stone.tick, 1 / 30)
+    stones.append(stone)
+
+#po zmáčknutí klávesy se zapíše do dat
+@window.event
+def on_key_press(data, mod):
+    global klavesy
+    klavesy.add(data)
+    
+#po spuštění klávesy se data vymažou
+@window.event
+def on_key_release(data, mod):
+    global klavesy
+    klavesy.remove(data)
 
 
 @window.event
